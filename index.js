7845fcec53288c27601d77c674169b2159726d25
Swatijha451 <swatijha451@gmail.com>
2024-05-14 18:47:03 +0530

    used RESTful APIs to add functionality to the project

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app= express();
-const port =8080;
 const path=require("path");
 const { v4: uuidv4 } = require('uuid');
 const methodOverride=require("method-override");
+const app= express();
+const port =8080;
+
 
 
 app.use(methodOverride("_method"));
@@ -58,10 +59,15 @@ app.patch("/posts/:id",(req,res)=>{
     res.redirect("/posts");
 
 })
-app.get("posts/:id/edit",(req,res) => {
+app.get("/posts/:id/edit",(req,res) => {
     let {id}=req.params;
     let post= posts.find((p)=>id===p.id);
     res.render("edit.ejs",{post});
+})
+app.delete("/posts/:id", (req,res)=>{
+    let{id} =req.params;
+    posts=posts.filter((p)=>id!==p.id);
+    res.redirect("/posts")
 })
 
 app.listen (port, ()=>{
